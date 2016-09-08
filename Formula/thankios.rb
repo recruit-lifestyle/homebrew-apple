3de68ac1f1c5070d6da0d8eb805ebb55fe6d6c35
Yuki Nagai <ynagai@r.recruit.co.jp>
2016-09-08 13:08:37 +0900

    [Thankios] Fix homepage url.

--- Formula/thankios.rb
+++ Formula/thankios.rb
@@ -1,6 +1,6 @@
 class Thankios < Formula
   desc "Acknowlegements Settings.bundle generator"
-  homepage "https://github.com/uny/Thankios"
+  homepage "https://github.com/recruit-lifestyle/Thankios"
   url "https://github.com/recruit-lifestyle/Thankios/archive/v2.1.0.tar.gz"
   version "2.1.0"
   sha256 "2a6d20bfcd4b3c38dd277c62b58c6eff1601a14c1c1f2836771db723f96b7e3e"
